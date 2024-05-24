f8232bff7f65d61c8c126a508ea9bd7a41d57fc6
Hossein Tavakoli <97765315+MHosseinTavakoliD@users.noreply.github.com>
2024-05-24 08:42:41 -0400

    Update settings.py remove credentials

--- Smartmdss/settings.py
+++ Smartmdss/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s_a$r!f%4wyx+eh@7&8yq!8xx8m1rk4$rhh=al6j4%g!8loegh'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
